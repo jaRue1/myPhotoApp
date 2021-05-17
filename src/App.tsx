00a15c72de5d6d934b606227219c7afeaf0bd531
jaRue1 <jaruejohnson@outlook.com>
2021-05-17 10:56:57 -0400

    added my photo component code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import Hello from './components/Hello'
+import Photo from './components/Photos'
 import './App.global.css';
 
 
@@ -13,6 +14,7 @@ export default function App() {
         <NavLink to = '/photo' exact> Photo </NavLink>
       </nav>
       <Switch>
+        <Route path="/photo" component={Photo}/>
         <Route exact path="/" component={Hello} />
       </Switch>
     </Router>
